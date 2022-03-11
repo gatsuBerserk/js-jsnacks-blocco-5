e786460118c05057387121e033e4ee2146a703b6
gatsuBerserk <berserkdev@outlook.it>
2022-03-11 16:51:16 +0100

    snack 4

---
+++ snack4/js/script.js
@@ -0,0 +1,44 @@
+/**
+ * 
+ * 
+    Crea un array di oggetti che rappresentano degli animali.
+    Ogni animale ha un nome, una famiglia e una classe.
+    Es:
+
+    [
+        { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+        { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+        { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+    ]
+
+    Crea un nuovo array con la lista dei mammiferi.
+
+ * 
+ */ 
+
+
+    const animali = [ 
+        {   nome: 'leone', 
+            famiglia: 'felidi', 
+            classe: 'mammiferi' 
+        }, 
+        {   nome: 'cane', 
+            famiglia: 'canidi', 
+            classe: 'mammiferi' 
+        }, 
+        {   nome: 'gallina', 
+            famiglia: 'fasianidi', 
+            classe: 'uccelli' 
+        }, 
+        
+    ]; 
+
+    const mammiferi = animali.filter((element) =>{
+        if (element.classe == "mammiferi"){
+            return true
+        }else{
+            return false
+        }
+    }); 
+
+    console.table(mammiferi)
